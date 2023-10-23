319b0313a3b803e29f0f86ad2233ab03603ca0f7
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-10-23 14:02:11 -0700

    notes

--- promises.js
+++ promises.js
@@ -1,53 +1,82 @@
-/* ============================== Phase 1 ============================== */
-/* -------------------------- exploring async -------------------------- */
+/* ===== Phase 1 ===== */
+/* ---- exploring async ---- */
 
 // Your code here
+function num1() {
+  return 1;
+}
+async function num2() {
+  return 2;
+}
+
+// console.log('num1', num1());
+// console.log('num2', num2());
 
 
 
-/* ============================== Phase 2 ============================== */
-/* -------------------------- exploring await -------------------------- */
+// num2().then(result => console.log(result));
+/* ===== Phase 2 ===== */
+/* ---- exploring await ---- */
 
 // Your code here
+async function waiting() {
+  const value = await num2();
+  console.log('waiting', value);
+}
+// waiting();
 
 
-
-/* ============================== Phase 3 ============================== */
+/* ===== Phase 3 ===== */
 /* --------------------- creating a custom Promise --------------------- */
 
 // Your code here
+async function waitForMyPromise() {
+  const promise = new Promise((resolve) => {
+      setTimeout(() => {
+          resolve('done!!!');
+      }, 1000)
+  });
+
+  const result = await promise;
+  console.log('my promise is', result);
+}
+waitForMyPromise();
 
 
 
-/* ============================== Phase 4 ============================== */
-/* -------------------------- exploring then --------------------------- */
+/* ===== Phase 4 ===== */
+/* ---- exploring then ----- */
 
 // Your code here
+new Promise((resolve) => {
+  setTimeout(()=>{
+    resolve('done');
+  }, 500)
+}).then(x => console.log('then my other promise is finally', x)) ;
 
 
-
-/* ============================== Phase 5 ============================== */
+/* ===== Phase 5 ===== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
 // Your code here
 
 
 
-/* ============================== Phase 6 ============================== */
+/* ===== Phase 6 ===== */
 /* -------------------- exploring reject and .catch -------------------- */
 
 // Your code here
 
 
 
-/* ============================== Phase 7 ============================== */
+/* ===== Phase 7 ===== */
 /* ---------------- exploring async/await and try/catch ---------------- */
 
 // Your code here
 
 
 
-/* ============================== Phase 8 ============================== */
+/* ===== Phase 8 ===== */
 /* -------------------- Promises are asynchronous! --------------------- */
 
 // Your code here
